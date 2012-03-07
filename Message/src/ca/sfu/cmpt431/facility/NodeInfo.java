8ce500781317f1444882db36b27c0a589fa1435c
Yao Xie <qddpx@163.com>
2012-03-07 15:05:13 -0800

    ok

--- Message/src/ca/sfu/cmpt431/facility/NodeInfo.java
+++ Message/src/ca/sfu/cmpt431/facility/NodeInfo.java
@@ -1,5 +1,20 @@
 package ca.sfu.cmpt431.facility;
 
 public class NodeInfo {
-
+	
+	private int id;
+	private String ip;
+	
+	public NodeInfo(int id, String ip) {
+		this.id = id;
+		this.ip = ip;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getIp() {
+		return ip;
+	}
 }
