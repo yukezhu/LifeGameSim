eeb6f2880b1e6b520bd6af20e6d070034e882028
Luna Lu <luchangl@emerald.csil.sfu.ca>
2012-03-07 15:48:46 -0800

    server3:48

--- GameOfLifeServer/src/ca/sfu/server/Server.java
+++ GameOfLifeServer/src/ca/sfu/server/Server.java
@@ -53,13 +53,14 @@ public class Server{
 				System.out.println(status);
 				m = Receiver.getNextMessageWithIp();
 				switch(status) {
-					//waiting for adding
+					//waiting for first client
 					case 0:
-						handleNewAdding(m);
+						handleNewAdding(m,1);
 						handlePending();
 						status = 1;
 						break;
-					//waiting for confirm
+					//waiting for confirm from first client
+					//send it the outfit
 					case 1:
 						handleConfirm(m, 0);
 						break;
@@ -162,22 +163,35 @@ public class Server{
 	}
 	
 	//store all the adding request into an array
-	protected void handleNewAdding(MessageWithIp m) throws IOException{
+	protected boolean handleNewAdding(MessageWithIp m, int nextStatus) throws IOException{
 		//check if m is a new adding request message
 		Message msg = (Message) m.extracMessage();
 		if(msg.getMessageCode()==MessageCodeDictionary.JOIN_REQUEST){
 			JoinRequestMsg join = (JoinRequestMsg)m.extracMessage();
 			newClientSender.add(new MessageSender(m.getIp(), join.getClientPort()));
 			System.out.println("adding new to pending");
+			//if it is a new adding request, we need to go to nextStatus
+			//most time it should be the same status
+			status = nextStatus;
+			return true;
 		}
-		return;
+		return false;
 	}
 	
-	
+	//deal with the pending adding request
+	//manage the heap
 	protected void handlePending() throws IOException{
 		while(!newClientSender.isEmpty()){
 			int cid = regedClientSender.size();
+			//manage the heap
+			if(cid!=0){ //not the first client
+				Comrade c = regedClientSender.get(0); //get it down one level
+				regedClientSender.remove(0);
+				regedClientSender.add(c);
+			}
 			regedClientSender.add(new Comrade(cid, newClientSender.get(0)));
+			
+			//remove the pending one
 			newClientSender.remove(0);
 			regedClientSender.get(cid).sender.sendMsg(new JoinConfirmMsg(cid));
 			waiting4confirm++;
