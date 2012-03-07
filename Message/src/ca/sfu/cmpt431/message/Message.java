78f68b490605249effc0d1164dad6cf6c1d5e2b5
Yao Xie <qddpx@163.com>
2012-03-07 13:58:48 -0800

    addmessage

---
+++ Message/src/ca/sfu/cmpt431/message/Message.java
@@ -0,0 +1,10 @@
+package ca.sfu.cmpt431.message;
+
+import java.io.Serializable;
+
+public class Message implements Serializable{
+	
+	private int clientId;
+	private int MessageCode;
+
+}
---
+++ Message/src/ca/sfu/cmpt431/message/MessageDictionary.java
@@ -0,0 +1,7 @@
+package ca.sfu.cmpt431.message;
+
+public class MessageDictionary {
+
+	public final static int SB_MESSAGE = 0;
+	
+}
