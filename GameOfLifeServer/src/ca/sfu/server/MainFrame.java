a1922375d3074b0f9ca30f399d7de68e3dcd79e4
Yuke Zhu <zyk0902@gmail.com>
2012-03-07 20:22:48 -0800

    Update UI

--- GameOfLifeServer/src/ca/sfu/server/MainFrame.java
+++ GameOfLifeServer/src/ca/sfu/server/MainFrame.java
@@ -1,9 +1,14 @@
 package ca.sfu.server;
 
 import java.awt.Color;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import ca.sfu.cmpt431.facility.Board;
+import ca.sfu.cmpt431.facility.BoardOperation;
 
 public class MainFrame extends JFrame {
 
@@ -16,15 +21,38 @@ public class MainFrame extends JFrame {
 	public MainFrame()
 	{
 		super();
+		this.setSize(480, 480);
+		
+		Board board = new Board(1000, 1000);
+		BoardOperation.Randomize(board, 0.3);
+		AutomataPanel automataPanel = new AutomataPanel();
+		automataPanel.setCellSize(2);
+		automataPanel.setBoard(board);
+		
+		infoPanel = new JPanel();
+		clientPanel = new JPanel();
+		
 		infoPanel.setBackground(Color.RED);
 		clientPanel.setBackground(Color.GREEN);
 		automataPanel.setBackground(Color.GRAY);
-		this.setContentPane(automataPanel);
+		setContentPane(automataPanel);
+		this.setVisible(true);
+		
+		addWindowListener(windowAdapter);
 	}
 	
 	public static void main(String[] args) {
 		
+		MainFrame frame = new MainFrame();
 		
 	}
+	
+	WindowAdapter windowAdapter = new WindowAdapter() {
+		public void windowClosing(WindowEvent e)
+		{
+			// Store data if necessary
+			System.exit(0);
+		}
+	};
 
 }
