c51ec69ac7bf95df3308f93ba48064aee49a0036
Yuke Zhu <zyk0902@gmail.com>
2012-03-07 19:43:24 -0800

    Server

--- GameOfLifeServer/src/ca/sfu/server/LifeGameServer.java
+++ GameOfLifeServer/src/ca/sfu/server/LifeGameServer.java
@@ -9,7 +9,6 @@ public class LifeGameServer extends JFrame {
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException
 	{
-		
 		Server s1 = new Server();
 		s1.startServer();
 	}
