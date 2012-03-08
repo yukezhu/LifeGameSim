3b7fc5f1d79554fda42c05d6ab1f070159d179bd
Yao Xie <qddpx@163.com>
2012-03-07 21:13:41 -0800

    xieyao 9:13

---
+++ Message/src/ca/sfu/cmpt431/message/regular/RegularUpdateNeighbourMsg.java
@@ -0,0 +1,21 @@
+package ca.sfu.cmpt431.message.regular;
+
+import ca.sfu.cmpt431.message.Message;
+import ca.sfu.cmpt431.message.MessageCodeDictionary;
+
+public class RegularUpdateNeighbourMsg extends Message {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public int [] pos;
+	public int port;
+	public String ip;
+
+	public RegularUpdateNeighbourMsg(int cid, int [] pos, int port, String ip) {
+		super(cid, MessageCodeDictionary.REGULAR_UPDATE_NEIGHBOUR);
+		this.pos = pos;
+		this.port = port;
+		this.ip = ip;
+	}
+
+}
