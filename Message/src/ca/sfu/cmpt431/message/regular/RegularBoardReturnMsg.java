07a5cca56e1eea634aa435bf6958c3d8d0e3ba1a
Luna Lu <luchangl@aqua.csil.sfu.ca>
2012-03-10 14:52:43 -0800

    board2:15

--- Message/src/ca/sfu/cmpt431/message/regular/RegularBoardReturnMsg.java
+++ Message/src/ca/sfu/cmpt431/message/regular/RegularBoardReturnMsg.java
@@ -8,10 +8,14 @@ public class RegularBoardReturnMsg extends Message{
 
 	private static final long serialVersionUID = 1L;
 
+	public int top;
+	public int left;
 	public Board board;
 	
-	public RegularBoardReturnMsg(int cid, Board newBoard) {
+	public RegularBoardReturnMsg(int cid, int top, int left, Board newBoard) {
 		super(cid, MessageCodeDictionary.REGULAR_BOARD_RETURN);
+		this.top = top;
+		this.left = left;
 		board = newBoard;
 	}
 	
