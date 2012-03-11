507585d344e2bbc203988875fb7a6e6d3cb61c3f
Yuke Zhu <yukez@azure.csil.sfu.ca>
2012-03-10 21:01:43 -0800

    ControlPanel

--- GameOfLifeServer/src/ca/sfu/server/ControlPanel.java
+++ GameOfLifeServer/src/ca/sfu/server/ControlPanel.java
@@ -1,13 +1,24 @@
 package ca.sfu.server;
 
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
 
 public class ControlPanel extends JFrame {
 
+	private static final long serialVersionUID = 1L;
+
 	public ControlPanel()
 	{
-		setSize(200, 200);
+		setSize(300, 300);
+		setTitle("Control Panel");
 	}
+	
+	@Override
+	public void paint(Graphics g) {
+		
+	}
+	
 	
 	public static void main(String args[])
 	{
