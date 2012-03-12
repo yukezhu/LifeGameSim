a32bb602774b1c8125f95afb6c8055bb8c7c3e7b
Yao Xie <xieyaox@auburn.csil.sfu.ca>
2012-03-12 13:05:56 -0700

    client debug

--- GameOfLifeClient/src/ca/sfu/client/Client.java
+++ GameOfLifeClient/src/ca/sfu/client/Client.java
@@ -433,7 +433,6 @@ public class Client {
 	private void computeAndReport() throws IOException {
 		BoardOperation.NextMoment(outfit.myBoard, null, null, null, null, false, false, false, false);
 		server.sender.sendMsg(new RegularBoardReturnMsg(outfit.myId, outfit.top, outfit.left, outfit.myBoard));
-		BoardOperation.Print(outfit.myBoard);
 		borderCount = 0;
 	}
 	
