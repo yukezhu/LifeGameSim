f444b17ebb486ae8bdfd94facea949194dab06c8
Yuke Zhu <zyk0902@gmail.com>
2012-03-14 13:04:14 -0700

    Server side

--- Message/src/ca/sfu/network/MessageSender.java
+++ Message/src/ca/sfu/network/MessageSender.java
@@ -42,10 +42,7 @@ public class MessageSender{
 		out.flush();
 		byte[] arr = bOut.toByteArray();
 		ByteBuffer bb = ByteBuffer.wrap(arr);
-
-		System.out.println("sending message of size " + arr.length);
-		System.out.println(arr);
-		
+		System.out.println("sending message of size " + arr.length);	
 		out.close();
 		socketChannel.write(bb);
 	}
