2c2435d297eff17a489be98698e2ebb81e819f56
Yuke Zhu <zyk0902@gmail.com>
2012-03-14 21:01:55 -0700

    Server side

--- GameOfLifeServer/src/ca/sfu/server/LifeGameServer.java
+++ GameOfLifeServer/src/ca/sfu/server/LifeGameServer.java
@@ -1,11 +1,7 @@
 package ca.sfu.server;
 import java.io.IOException;
 
-import javax.swing.JFrame;
-
-public class LifeGameServer extends JFrame {
-
-	private static final long serialVersionUID = 1L;
+public class LifeGameServer {
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException
 	{
