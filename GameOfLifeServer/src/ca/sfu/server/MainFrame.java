2b888e2c523c87c035324072d54353c2e2692404
Yuke Zhu <zyk0902@gmail.com>
2012-03-15 21:55:48 -0700

    New about dialog

--- GameOfLifeServer/src/ca/sfu/server/MainFrame.java
+++ GameOfLifeServer/src/ca/sfu/server/MainFrame.java
@@ -5,6 +5,9 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
@@ -137,39 +140,39 @@ public class MainFrame extends JFrame {
 		about.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				Object[] options = { "OK" };
 				JLabel label = new JLabel();
-			    Font font = label.getFont();
+				Font font = label.getFont();
 
-			    // create some css from the label's font
-			    StringBuffer style = new StringBuffer("font-family:" + font.getFamily() + ";");
-			    style.append("font-weight:" + (font.isBold() ? "bold" : "normal") + ";");
-			    style.append("font-size:" + font.getSize() + "pt;");
+				// create some css from the label's font
+				StringBuffer style = new StringBuffer("font-family:" + font.getFamily() + ";");
+				style.append("font-weight:" + (font.isBold() ? "bold" : "normal") + ";");
+				style.append("font-size:" + font.getSize() + "pt;");
 
-			    // html content
-			    String text1 = "<html><body><p><strong><font size=\"5\" face=\"arial\" color=\"black\">Game of life</font></strong></p>" +
-			    			   "<p>version:1.1</p><p>CMPT 431 group project:</p>"    													 +
-			    			   "<p>We propose a distributed cellular automaton simulation application, called world  of cell.</p>"       +  
-			    			   "<p>Author: Yuke Zhu, Luna Lu, Yang Liu, Yao Xie, Xiaying Peng</p>"                                       +
-			    			   "<p>Sound interesting? <a href=\"www.google.com\">Get involved!</a></p></body></html>";
-			    JEditorPane ep = new JEditorPane("text/html", text1);
+				// html content
+				String text1 = "<html><body><p><strong><font size=\"5\" face=\"arial\" color=\"black\">Game of Life</font></strong></p>" +
+						"<p><i>Version 1.1</i></p><p><i>School of Computing Science, Simon Fraser University</i></p>"    													 +
+						"<p>Distributed cellular automaton simulation application, called world  of cell.</p>"       +  
+						"<p><b>Author:</b> Yuke Zhu, Luna Lu, Yang Liu, Yao Xie, Xiaying Peng</p>"                                       +
+						"<p>Sound interesting? <a href=\"https://github.com/leafpicker/LifeGameSim\">Get involved!</a></p></body></html>";
+				JEditorPane ep = new JEditorPane("text/html", text1);
 
-			    // handle link events
-			    ep.addHyperlinkListener(new HyperlinkListener()
-			    {
-			        @Override
-			        public void hyperlinkUpdate(HyperlinkEvent e)
-			        {
-			            //if (e.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED))
-			                //ProcessHandler.launchUrl(e.getURL().toString()); // roll your own link launcher or use Desktop if J6+
-			        }
-			    });
+				// handle link events
+				ep.addHyperlinkListener(new HyperlinkListener()
+				{
+					@Override
+					public void hyperlinkUpdate(HyperlinkEvent e)
+					{
+						if (e.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED))
+							openURL(e.getURL().toString());
+						//ProcessHandler.launchUrl(e.getURL().toString()); // roll your own link launcher or use Desktop if J6+
+					}
+				});
 
-			    ep.setEditable(false);
-			    ep.setBackground(label.getBackground());
+				ep.setEditable(false);
+				ep.setBackground(label.getBackground());
 
-			    // show
-			    JOptionPane.showMessageDialog(null, ep);
+				// show
+				JOptionPane.showMessageDialog(null, ep);
 
 			}});
 		exit.addActionListener(new ActionListener() {
@@ -197,6 +200,40 @@ public class MainFrame extends JFrame {
 	public Board getBoard()
 	{
 		return this.board;
+	}
+
+	public static void openURL(String url) {
+		try {
+			browse(url);
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Error attempting to launch web browser:\n" + e.getLocalizedMessage());
+		}
+	}
+
+	/**
+	 * Browse website from default browser with multiple OS compatibility
+	 */
+	private static void browse(String url) throws ClassNotFoundException, IllegalAccessException,
+	IllegalArgumentException, InterruptedException, InvocationTargetException, IOException,
+	NoSuchMethodException {
+		String osName = System.getProperty("os.name", "");
+		if (osName.startsWith("Mac OS")) {
+			Class<?> fileMgr = Class.forName("com.apple.eio.FileManager");
+			Method openURL = fileMgr.getDeclaredMethod("openURL", new Class[] { String.class });
+			openURL.invoke(null, new Object[] { url });
+		} else if (osName.startsWith("Windows")) {
+			Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+		} else { // assume Unix or Linux
+			String[] browsers = { "firefox", "opera", "konqueror", "epiphany", "mozilla", "netscape" };
+			String browser = null;
+			for (int count = 0; count < browsers.length && browser == null; count++)
+				if (Runtime.getRuntime().exec(new String[] { "which", browsers[count] }).waitFor() == 0)
+					browser = browsers[count];
+			if (browser == null)
+				throw new NoSuchMethodException("Could not find web browser");
+			else
+				Runtime.getRuntime().exec(new String[] { browser, url });
+		}
 	}
 
 	public static void main(String[] args) {
