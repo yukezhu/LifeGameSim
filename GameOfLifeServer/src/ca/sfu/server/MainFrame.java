258f9507b3de2324929d6d15bdcb3c77a8f1864e
gigimushroom <xiayingp@gmail.com>
2012-03-16 12:24:21 +0800

    adding about feature

--- GameOfLifeServer/src/ca/sfu/server/MainFrame.java
+++ GameOfLifeServer/src/ca/sfu/server/MainFrame.java
@@ -137,7 +137,6 @@ public class MainFrame extends JFrame {
 		about.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				System.out.println("I am clicked!!!");
 				Object[] options = { "OK" };
 				JLabel label = new JLabel();
 			    Font font = label.getFont();
