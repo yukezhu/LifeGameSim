67815666f33c3fde93dce8b9c8b4645ccb42e3aa
Yao Xie <xieyaox@cinnabar.csil.sfu.ca>
2012-03-17 17:48:31 -0700

    client

--- GameOfLifeClient/src/ca/sfu/client/Client.java
+++ GameOfLifeClient/src/ca/sfu/client/Client.java
@@ -1,7 +1,5 @@
 package ca.sfu.client;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
