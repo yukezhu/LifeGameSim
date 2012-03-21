087098e39335ef0623d4709c89e872c590187fbf
Luna Lu <luchangl@cerulean.csil.sfu.ca>
2012-03-21 14:49:31 -0700

    buffersize2:49

--- Message/src/ca/sfu/network/MessageCompressor.java
+++ Message/src/ca/sfu/network/MessageCompressor.java
@@ -9,7 +9,7 @@ import java.util.zip.Inflater;
 public class MessageCompressor {
 	
 //	public static final int BufferSize = 54857600;
-	public static final int BufferSize = 16041606;
+	public static final int BufferSize = 65536;
 
 	public static byte[] compress(final byte[] input)
 	{
