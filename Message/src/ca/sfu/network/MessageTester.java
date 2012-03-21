7743eae73ade471704fc3227986508150a93c01a
Yuke Zhu <yukez@cerise.csil.sfu.ca>
2012-03-21 14:19:33 -0700

    Tester

--- Message/src/ca/sfu/network/MessageTester.java
+++ Message/src/ca/sfu/network/MessageTester.java
@@ -1,10 +1,24 @@
 package ca.sfu.network;
 
+import java.io.IOException;
+
 public class MessageTester {
 
-	public static void main(String[] args)
+	public static void main(String[] argv) throws IOException, InterruptedException
 	{
-		
+		String clientFlag = argv[0];
+		if(clientFlag.equals("client"))
+		{
+			MessageSender sender = new MessageSender("127.0.0.1", 5354);
+			sender.sendMsg("hello world");			
+		} else {
+			MessageReceiver receiver = new MessageReceiver(5354);
+			while(!receiver.isEmpty())
+			{
+				String s = (String) receiver.getNextMessageWithIp().extracMessage();
+				System.out.println(s);
+			}
+		}
 	}
 	
 }
