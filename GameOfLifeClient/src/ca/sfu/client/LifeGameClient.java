e64a31372929abecea1259db0f94d05ce779e9b5
Yuke Zhu <zyk0902@gmail.com>
2012-03-21 18:26:21 -0700

    Client UI

--- GameOfLifeClient/src/ca/sfu/client/LifeGameClient.java
+++ GameOfLifeClient/src/ca/sfu/client/LifeGameClient.java
@@ -4,18 +4,8 @@ public class LifeGameClient {
 
 	public static void main(String[] args)
 	{
-		try {
-			if(args.length < 2)
-			{
-				System.out.println("Usage: client.jar [server ip] [host ip]");
-			} else
-			{
-				Client c = new Client();
-				c.startClient(args[0], args[1]);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		MainFrame frame = new MainFrame();
+		frame.setVisible(true);
 	}
 	
 }
--- GameOfLifeClient/src/ca/sfu/client/MainFrame.java
+++ GameOfLifeClient/src/ca/sfu/client/MainFrame.java
@@ -1,32 +1,108 @@
 package ca.sfu.client;
 
-import java.awt.GridLayout;
+import java.awt.Container;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 
 public class MainFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 
-	public MainFrame()
-	{
-		GridLayout layout = new GridLayout(3, 3);
-		setLayout(layout);
-		
-		this.add(new JLabel("Client Ip: "));
-		
-		setSize(500, 500);
-		setVisible(true);
-		setTitle("Client");
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
+	/* Client object */
+	private Client client = new Client();
 	
+	/* New UI widgets */
+	JLabel clientIpLabel = new JLabel("Client IP");
+	JTextField clientIp = new JTextField(15);
+	JLabel serverIpLabel = new JLabel("Server IP");
+	JTextField serverIp = new JTextField(15);
+	JButton connectBtn = new JButton("Connect");
+	
+	boolean connected;
+	
+	public MainFrame() {
+
+		Container contentPane = getContentPane();
+
+		SpringLayout layout = new SpringLayout();
+		contentPane.setLayout(layout);
+
+		/* Add to content pane */
+		contentPane.add(clientIpLabel);
+		contentPane.add(clientIp);
+		contentPane.add(serverIpLabel);
+		contentPane.add(serverIp);
+		contentPane.add(connectBtn);
+		connectBtn.setFocusable(false);
+
+		/* Regular expression */
+//		String ValidIpAddressRegex = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.";
+//		String ValidHostnameRegex = "^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\\-]*[A-Za-z0-9])$";
+//		final String IPADDRESS_PATTERN = ValidIpAddressRegex + ValidHostnameRegex;
+		
+		/* Put UI constrains */
+		layout.putConstraint(SpringLayout.WEST, clientIpLabel, 15, SpringLayout.WEST, contentPane);
+		layout.putConstraint(SpringLayout.NORTH, clientIpLabel, 30, SpringLayout.NORTH, contentPane);
+		layout.putConstraint(SpringLayout.NORTH, clientIp, 30, SpringLayout.NORTH, contentPane);
+		layout.putConstraint(SpringLayout.WEST, clientIp, 80, SpringLayout.WEST, clientIpLabel);
+		
+		layout.putConstraint(SpringLayout.WEST, serverIpLabel, 15, SpringLayout.WEST, contentPane);
+		layout.putConstraint(SpringLayout.NORTH, serverIpLabel, 30, SpringLayout.NORTH, clientIpLabel);
+		layout.putConstraint(SpringLayout.NORTH, serverIp, 30, SpringLayout.NORTH, clientIp);
+		layout.putConstraint(SpringLayout.WEST, serverIp, 80, SpringLayout.WEST, serverIpLabel);
+
+		layout.putConstraint(SpringLayout.NORTH, connectBtn, 30, SpringLayout.NORTH, serverIpLabel);
+		layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, connectBtn, 0, SpringLayout.HORIZONTAL_CENTER, contentPane);
+		
+		/* Set listener */
+		connectBtn.addMouseListener(new MouseListener() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if(!connected)
+				{
+					try
+					{
+						System.out.println("Connecting...");
+						client.startClient(serverIp.getText(), clientIp.getText());
+						connectBtn.setText("Disconnect");
+					} catch(Exception e)
+					{
+						e.printStackTrace();
+					}
+				} else 
+				{
+//					client.disconnect(); /* Add disconnect function here */ 
+					connectBtn.setText("Connect");
+				}
+				connected = !connected;
+			}
+			@Override
+			public void mouseEntered(MouseEvent arg0) {}
+			@Override
+			public void mouseExited(MouseEvent arg0) {}
+			@Override
+			public void mousePressed(MouseEvent arg0) {}
+			@Override
+			public void mouseReleased(MouseEvent arg0) {}
+		});
+		
+		connected = false;
+		setSize(300, 180);
+		setTitle("Client");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);    
+	}  
+
 	public static void main(String[] argv)
 	{
 		@SuppressWarnings("unused")
 		MainFrame frame = new MainFrame();
-		
+
 	}
-	
+
 }
