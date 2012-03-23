d97d19542ec6c0c0695efc41aaff067405bcd899
Yuke Zhu <yukez@sanaga.csil.sfu.ca>
2012-03-22 20:48:21 -0700

    New Data processor

---
+++ DataProcessor.py
@@ -0,0 +1,29 @@
+from __future__ import division
+import pylab
+import sys
+
+
+st = 0
+ed = 0
+
+cnt = 1
+x = []
+y = []
+
+for line in sys.stdin:
+	if line.startswith('Time'):
+		line = line[line.find(':')+1:]
+		ed = float(line)
+		cycle = 20 * 1000 / (ed - st)
+		x += [cnt]
+		y += [cycle]
+		cnt = cnt + 1
+		#print x, y
+	elif line.startswith('Start time'):
+		line = line[12:]
+		st = float(line)
+	else:
+		continue
+
+pylab.plot(x, y)
+pylab.show()
